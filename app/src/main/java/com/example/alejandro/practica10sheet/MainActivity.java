1ba8f27607b2a1ab513101601325b70915055fc8
DESKTOP-7U6IS1N\Alejandro <a.santillana@aol.com>
2018-02-02 09:48:01 +0100

    Inicio

---
+++ app/src/main/java/com/example/alejandro/practica10sheet/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.alejandro.practica10sheet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
